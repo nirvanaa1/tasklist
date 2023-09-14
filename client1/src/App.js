1fbf9443979b1497297a55b8d89fb9291af3b509
nirvanaa1 <144881488+nirvanaa1@users.noreply.github.com>
2023-09-14 12:53:50 +0530

    Update App.js

--- client1/src/App.js
+++ client1/src/App.js
@@ -6,8 +6,6 @@ import Header from './components/Header';
 import TodoForm from './components/TodoForm';
 import Todos from './components/Todos';
 
-//import BackgroundSlider from './BackgroundSlider';
-
 
 function App() {
   return (
